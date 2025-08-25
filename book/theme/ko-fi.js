4cc7f5eee380eeefc73b62afc55394869e393a4f
green-threads <jakub.trumpes@gmail.com>
2025-08-25 16:54:27 +0200

    removed build directory

--- book/theme/ko-fi.js
+++
@@ -1,6 +0,0 @@
-kofiWidgetOverlay.draw('caukub', {
-  'type': 'floating-chat',
-  'floating-chat.donateButton.text': 'Support me',
-  'floating-chat.donateButton.background-color': '#00b9fe',
-  'floating-chat.donateButton.text-color': '#fff'
-});
